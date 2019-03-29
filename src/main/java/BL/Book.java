3eabbb596302eafd66dcaed7cf2c74bdd5d768b0
David Blümel <bludac16@htl-kaindorf.ac.at>
2019-03-29 12:37:35 +0100

    Implemented Class Book

--- src/main/java/BL/Book.java
+++ src/main/java/BL/Book.java
@@ -5,10 +5,37 @@
  */
 package BL;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  *
  * @author David
  */
 public class Book {
+    private String inputName;
+    private String text;
+
+    public Book(String inputName, String text) {
+        this.inputName = inputName;
+        this.text = text;
+    }
     
+    public HashMap<String,Integer> duplicateWords(){
+        List<String> list = Arrays.asList(text.split(" "));
+        Set<String> uniqueWords = new HashSet<String>(list);
+        
+        HashMap<String,Integer> words = new HashMap<String,Integer>();
+        
+        for (String word : uniqueWords) {
+            if(Collections.frequency(list,word) > 1){
+                words.put(word, Collections.frequency(list,word));
+            }
+        }
+        return words;
+    }
 }
